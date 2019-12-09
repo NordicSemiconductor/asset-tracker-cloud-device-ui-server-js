10964f6e6b358deac6a2c4e3ed49d085eac1ce08
Markus Tacker <Markus.Tacker@NordicSemi.no>
2019-12-09 15:53:58 +0100

    fix: add missing dependency

--- src/ui-server.ts
+++ src/ui-server.ts
@@ -1,5 +1,5 @@
 import * as http from 'http'
-import chalk from 'chalk'
+import * as chalk from 'chalk'
 import { portForDevice } from './portForDevice'
 import { server as WebSocketServer } from 'websocket'
 
